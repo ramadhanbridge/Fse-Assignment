d1e692decab9e5dfb6bd3a22ea6a7fe60f13ce06
Ramadhan <nyiringondoramadhan@gmail.com>
2024-01-27 13:45:33 +0200

    change style

--- BackEnd/helpers/jwt.js
+++ BackEnd/helpers/jwt.js
@@ -3,7 +3,7 @@ class JwtValidation
 {
 jwt_signin = (user_info) =>
  { 
-   let sign_info = jwt.sign({user_info}, process.env.PASS_KEY ,{ expiresIn:'1h'})
+   let sign_info = jwt.sign({user_info}, process.env.PASS_KEY)
      
    return sign_info;
  }
